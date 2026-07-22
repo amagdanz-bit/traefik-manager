2d23d043ca0f64e64c897fbaa90d376be12c473c
chr0nzz <187675356@users.noreply.github.com>
2026-07-22 16:31:34 -0400

    chore: bump version to 1.8.0

--- agent/main.go
+++ agent/main.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-const Version = "1.7.3"
+const Version = "1.8.0"
 
 type Config struct {
 	APIKey                 string
--- app.py
+++ app.py
@@ -25,7 +25,7 @@ from io import StringIO
 from cryptography.fernet import Fernet, InvalidToken
 
 GITHUB_REPO  = "chr0nzz/traefik-manager"
-APP_VERSION  = "1.7.3"
+APP_VERSION  = "1.8.0"
 
 
 LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
